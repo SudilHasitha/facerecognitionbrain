cfe238e07611d5ebe7eb4d0604bf2ae03eda0bd8
SudilHasitha <sudilhasithaa51@gmail.com>
2020-07-06 20:40:07 +0530

    Updates

---
+++ precache-manifest.5436e93467bcd29b08f7789c814d655a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "dd21cb44a658279914e7",
+    "url": "/facerecognitionbrain/static/css/main.8fc95074.chunk.css"
+  },
+  {
+    "revision": "dd21cb44a658279914e7",
+    "url": "/facerecognitionbrain/static/js/main.dbb27382.chunk.js"
+  },
+  {
+    "revision": "ce2d7a8efc8c8d84e679",
+    "url": "/facerecognitionbrain/static/js/runtime~main.2d683c34.js"
+  },
+  {
+    "revision": "7e1fd6ad8f5388a7624b",
+    "url": "/facerecognitionbrain/static/css/2.791af490.chunk.css"
+  },
+  {
+    "revision": "7e1fd6ad8f5388a7624b",
+    "url": "/facerecognitionbrain/static/js/2.9b1b621c.chunk.js"
+  },
+  {
+    "revision": "31f7e9431016a577c971ae8cceb3db63",
+    "url": "/facerecognitionbrain/index.html"
+  }
+];
